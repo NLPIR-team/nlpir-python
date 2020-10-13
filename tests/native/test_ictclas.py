6dc55440df2a49d25d8e919fbd5931089af88833
杨耀飞 <yangyaofei@gmail.com>
2020-10-13 10:53:17 +0800

    add more ictclas test

--- tests/native/test_ictclas.py
+++ tests/native/test_ictclas.py
@@ -24,12 +24,12 @@ def test_init_exit():
 
 def test_paragraph_process():
     ictclas = get_ictclas()
-    test_str_seg_pos = '法国/nsf 启蒙/vn 思想家/n 孟德斯/nrf 鸠/n 曾/d 说/v 过/uguo ：/wp “/wyz 一切/rz 有/vyou 权力/n ' \
+    test_str_seg_pos = '法国/nsf 启蒙/vn 思想家/n 孟德斯/nrf 鸠/n 曾/d 说/v 过/uguo ：/wm “/wyz 一切/rz 有/vyou 权力/n ' \
                        '的/ude1 人/n 都/d 容易/ad 滥用/v 权力/n ，/wd 这/rzv 是/vshi 一/m 条/q 千古/n 不/d 变/v 的/ude1 经' \
                        '验/n 。/wj 有/vyou 权力/n 的/ude1 人/n 直到/v 把/pba 权力/n 用到/v 极限/n 方可/d 休止/vi 。/wj ”/wyy' \
                        ' 另/rz 一/m 法国/nsf 启蒙/vn 思想家/n 卢/nr1 梭/ng 从/p 社会/n 契约/n 论/k 的/ude1 观点/n 出发/vi ' \
                        '，/wd 认为/v 国家/n 权力/n 是/vshi 公民/n 让/v 渡/v 其/rz 全部/m “/wyz 自然/n 权利/n ”/wyy 而/cc ' \
-                       '获得/v 的/ude1 ，/wd 他/rr 在/p 其/rz 名著/n 《/wkz 社会/n 契约/n 论/v 》/wky 中/f 写道/v ：/wp ' \
+                       '获得/v 的/ude1 ，/wd 他/rr 在/p 其/rz 名著/n 《/wkz 社会/n 契约/n 论/v 》/wky 中/f 写道/v ：/wm ' \
                        '“/wyz 任何/rz 国家/n 权力/n 无不/d 是/vshi 以/p 民众/n 的/ude1 权力/n （/wkz 权利/n ）/wky 让/v ' \
                        '渡/v 与/p 公众/n 认可/vi 作为/p 前提/n 的/ude1 ”/wyy 。/wj '
 
@@ -56,7 +56,7 @@ def test_paragraph_process_a():
 def test_file_process():
     ictclas = get_ictclas()
     test_result_filename = "native/test_result.txt"
-    ictclas.file_process(test_source_filename, test_result_filename, 1)
+    ictclas.file_process(os.path.abspath(test_source_filename), os.path.abspath(test_result_filename), 1)
     os.remove(test_result_filename)
 
 
@@ -69,6 +69,10 @@ def test_import_user_dict():
     ictclas.add_user_word("孟德斯鸠")
     assert test_str_seg_with_dict == ictclas.paragraph_process(test_str_1st)
     ictclas.del_usr_word("孟德斯鸠")
+    assert test_str_seg == ictclas.paragraph_process(test_str_1st)
+    ictclas.add_user_word("孟德斯鸠")
+    assert test_str_seg_with_dict == ictclas.paragraph_process(test_str_1st)
+    ictclas.clean_user_word()
     assert test_str_seg == ictclas.paragraph_process(test_str_1st)
 
     # test add and delete multi word with import_user_dict
@@ -84,19 +88,15 @@ def test_import_user_dict():
     # 导入词典对应文件为FieldDict.pdat FieldDict.pos 初始状态下位空,可以删除 这里测试是导入测试后将其删除
     ictclas.import_user_dict(user_dict_path, True)
     assert test_str_seg_with_dict == ictclas.paragraph_process(test_str_2nd)
-    os.remove(user_dict_path)
-    os.remove(os.path.join(PACKAGE_DIR, "Data/FieldDict.pdat"))
-    os.remove(os.path.join(PACKAGE_DIR, "Data/FieldDict.pos"))
-    os.remove(os.path.join(PACKAGE_DIR, "Data/UserDefinedDict.lst"))
-
-    # TODO
-    # ictclas.clean_user_word()
-    # save_the_usr_dic()
-    # import_user_dict(filename, overwrite)
+    try:
+        os.remove(user_dict_path)
+        os.remove(os.path.join(PACKAGE_DIR, "Data/FieldDict.pdat"))
+        os.remove(os.path.join(PACKAGE_DIR, "Data/FieldDict.pos"))
+        os.remove(os.path.join(PACKAGE_DIR, "Data/UserDefinedDict.lst"))
+    except FileNotFoundError as e:
+        logging.warning(e)
 
 
-#
-#
 def test_user_dict():
     ictclas = get_ictclas()
     test_str_seg = ictclas.paragraph_process(test_str_2nd)
@@ -136,13 +136,11 @@ def test_finer_segment():
 
 
 def test_frq_count():
-    test_str_frq = '权力/n/7#的/ude1/7#。/wj/3#”/wyy/3#，/wd/3#是/vshi/3#“/wyz/3#国家/n/2#：/wp/2#让/v/2#法国/nsf/2#有' \
-                   '/vyou/2#渡/v/2#其/rz/2#人/n/2#社会/n/2#权利/n/2#思想家/n/2#启蒙/vn/2#一/m/2#契约/n/2#这/rzv/1#条/q/' \
-                   '1#千古/n/1#不/d/1#变/v/1#经验/n/1#滥用/v/1#直到/v/1#把/pba/1#用到/v/1#极限/n/1#方可/d/1#休止/vi/1#容' \
-                   '易/ad/1#另/rz/1#卢/nr1/1#梭/ng/1#从/p/1#都/d/1#一切/rz/1#论/k/1#观点/n/1#出发/vi/1#认为/v/1#过/uguo' \
-                   '/1#公民/n/1#说/v/1#曾/d/1#鸠/n/1#全部/m/1#自然/n/1#孟德斯/nrf/1#而/cc/1#获得/v/1#他/rr/1#在/p/1#名著' \
-                   '/n/1#《/wkz/1#论/v/1#》/wky/1#中/f/1#写道/v/1#任何/rz/1#无不/d/1#以/p/1#民众/n/1#（/wkz/1#）/wky/1#' \
-                   '与/p/1#公众/n/1#认可/vi/1#作为/p/1#前提/n/1#'
+    test_str_frq = '权力/n/7#是/vshi/3#思想家/n/2#权利/n/2#渡/v/2#让/v/2#有/vyou/2#法国/nsf/2#人/n/2#国家/n/2#契约/n/2#' \
+                   '启蒙/vn/2#一/m/2#社会/n/2#变/v/1#经验/n/1#直到/v/1#用到/v/1#极限/n/1#休止/vi/1#卢/nr1/1#梭/ng/1#千古/n/1#' \
+                   '滥用/v/1#观点/n/1#出发/vi/1#认为/v/1#容易/ad/1#公民/n/1#说/v/1#鸠/n/1#全部/m/1#自然/n/1#孟德斯/nrf/1#' \
+                   '获得/v/1#名著/n/1#论/v/1#写道/v/1#民众/n/1#公众/n/1#认可/vi/1#前提/n/1#'
+
     ictclas = get_ictclas()
     assert test_str_frq == ictclas.word_freq_stat(test_str)
     assert ictclas.file_word_freq_stat(test_source_filename)
@@ -152,13 +150,15 @@ def test_get_eng_word_origin():
     test_str_eng = "We hold these truths to be self-evident, that all men are created equal, that they are endowed " \
                    "by their Creator with certain unalienable Rights, that among these are Life, Liberty and the " \
                    "pursuit of Happiness."
-    test_str_eng_result = "We hold this truth to be self-evident, that all man be create equal, that they be endow" \
-                          " by their Creator with certain unalienable Rights, that among this be Life, Liberty and" \
-                          " the pursuit of Happiness."
+    test_str_eng_result = "we hold this truth to be self-evident, that all man be create equal, that they be endow" \
+                          " by their creator with certain unalienable rights, that among this be life, liberty and" \
+                          " the pursuit of happiness."
     ictclas = get_ictclas()
     for w, r in zip(test_str_eng.split(), test_str_eng_result.split()):
         assert r == ictclas.get_eng_word_origin(w)
 
 
 def test_last_error_msg():
-    assert get_ictclas().get_last_error_msg()
+    msg = get_ictclas().get_last_error_msg()
+    logging.info(msg)
+    assert msg is not None
