5abc02622552d838dc69de30bfe9c587a02e81b1
杨耀飞 <yangyaofei@gmail.com>
2020-10-13 16:59:19 +0800

    bugfix for packaging

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -1,5 +1,15 @@
 # coding : utf-8
 import os
+import typing
 
-__version__ = "0.0.1"
+__version__ = "0.0.1b"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def clean_logs(data_path: typing.Optional[str] = None, include_current: bool = False):
+    """
+    TODO
+    :param data_path:
+    :param include_current:
+    :return:
+    """
