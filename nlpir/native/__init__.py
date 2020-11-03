c6b8f37b2b4a1627e19eefcf9e96d922deb07fc3
杨耀飞 <yangyaofei@gmail.com>
2020-11-03 10:48:36 +0800

    bugfix

--- nlpir/native/__init__.py
+++ nlpir/native/__init__.py
@@ -2,4 +2,7 @@ from .ictclas import ICTCLAS
 from .nlpir_base import NLPIRException
 from .new_word_finder import NewWordFinder
 from .key_extract import KeyExtract
+from .classifier import Classifier
+from .sentiment import SentimentAnalysis, SentimentNew
+from .summary import Summary
 from .nlpir_base import UNKNOWN_CODE, GBK_CODE, UTF8_CODE, BIG5_CODE, GBK_FANTI_CODE, UTF8_FANTI_CODE
