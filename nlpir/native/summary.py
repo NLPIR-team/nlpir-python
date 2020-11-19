75d654acb19ea9fc0b23a19556ee925e0c8346c1
杨耀飞 <yangyaofei@gmail.com>
2020-11-19 16:57:09 +0800

    documentation

--- nlpir/native/summary.py
+++ nlpir/native/summary.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 from nlpir.native.nlpir_base import NLPIRBase
-from ctypes import c_bool, c_char, c_char_p, c_double, c_int, c_uint, POINTER, Structure, byref, c_float
-import typing
+from ctypes import c_char_p, c_int, byref, c_float, create_string_buffer
 import os
 
 
@@ -15,11 +14,7 @@ class Summary(NLPIRBase):
     @NLPIRBase.byte_str_transform
     def init_lib(self, data_path: str, encode: int, license_code: str) -> int:
         """
-        功能：初始化
-        参数：sLicenseCode - 授权码
-            sPath-default path
-        返回：true - 成功；false - 失败
-        备注：在进程中此函数必须在其他函数之前调用（只需执行一次）
+        Call **DS_Init**
 
         :param data_path:
         :param encode:
@@ -31,6 +26,7 @@ class Summary(NLPIRBase):
     @NLPIRBase.byte_str_transform
     def exit_lib(self) -> bool:
         """
+        Call **DS_Exit**
 
         :return:
         """
@@ -40,6 +36,7 @@ class Summary(NLPIRBase):
     @NLPIRBase.byte_str_transform
     def get_last_error_msg(self) -> str:
         """
+        Call **DS_GetLastErrMsg**
 
         :return:
         """
@@ -48,18 +45,16 @@ class Summary(NLPIRBase):
     @NLPIRBase.byte_str_transform
     def single_doc(self, text: str, sum_rate: float = 0.0, sum_len: int = 250, html_tag_remove: int = 0):
         """
-        功能：生成单文档摘要
-        参数：sText	    -[IN] 文档内容
-            fSumRate	-[IN] 文档摘要占原文百分比（为0.00则不限制）
-            iSumLen		-[IN] 用户限定的摘要长度  （为0则不限制）
-            bHtmlTagRemove-[IN] 是否需要对原文进行Html标签的去除
-        返回：摘要字符串；出错返回空串
-        备注：在进程中此函数可以执行多次
-        :param text:
-        :param sum_rate:
-        :param sum_len:
-        :param html_tag_remove:
-        :return:
+        Call **DS_SingleDoc**
+
+        生成单文档摘要, make summarization
+
+        :param str text: 文档内容 text content
+        :param float sum_rate: 文档摘要占原文百分比(为0.00则不限制）
+            the percentage of summarization length comparing to original text (0.00 represent no limit)
+        :param int sum_len: 用户限定的摘要长度(为0则不限制）The max len of summarization(0 will no limit)
+        :param bool html_tag_remove: 是否需要对原文进行Html标签的去除 remove the html tag or not
+        :return: 摘要字符串；出错返回空串 the summarization content, get null string if occurs error.
         """
         return self.get_func("DS_SingleDoc", [c_char_p, c_float, c_int, c_int], c_char_p)(
             text, c_float(sum_rate), sum_len, html_tag_remove
@@ -68,42 +63,38 @@ class Summary(NLPIRBase):
     @NLPIRBase.byte_str_transform
     def single_doc_e(self, text: str, sum_rate: float = 0.0, sum_len: int = 250, html_tag_remove: int = 0):
         """
-        功能：生成单文档摘要该函数支持多线程，是多线程安全的
-        参数：sResult    -[IN] 摘要内容
-            sText	    -[IN] 文档内容
-            fSumRate	-[IN] 文档摘要占原文百分比（为0.00则不限制）
-            iSumLen		-[IN] 用户限定的摘要长度  （为0则不限制）
-            bHtmlTagRemove-[IN] 是否需要对原文进行Html标签的去除
-        返回：摘要字符串；出错返回空串
-        备注：在进程中此函数可以执行多次
+        Call **DS_SingleDocE**
 
-        :param text:
-        :param sum_rate:
-        :param sum_len:
-        :param html_tag_remove:
-        :return:
+        生成单文档摘要该函数支持多线程，是多线程安全的, make summarization with threading safe
+
+        :param str text: 文档内容 text content
+        :param float sum_rate: 文档摘要占原文百分比(为0.00则不限制）
+            the percentage of summarization length comparing to original text (0.00 represent no limit)
+        :param int sum_len: 用户限定的摘要长度(为0则不限制）The max len of summarization(0 will no limit)
+        :param bool html_tag_remove: 是否需要对原文进行Html标签的去除 remove the html tag or not
+        :return: 摘要字符串；出错返回空串 the summarization content, get null string if occurs error.
         """
-        result = c_char_p()
+        buffer_len = int(len(text) * 3 * (sum_rate if sum_rate > 0.0 else 1))
+        buffer_len = sum_len if sum_len < buffer_len else buffer_len
+        result = create_string_buffer(buffer_len * 4)
         result_2 = self.get_func("DS_SingleDocE", [c_char_p, c_char_p, c_float, c_int, c_int])(
-            byref(result), text, sum_rate, sum_len, html_tag_remove
+            result, text, c_float(sum_rate), sum_len, html_tag_remove
         )
-        return result, result_2
+        return result.value, result_2
 
     @NLPIRBase.byte_str_transform
     def file_process(self, text_filename: str, sum_rate: float = 0.0, sum_len: int = 250, html_tag_remove: int = 0):
         """
-        功能：生成单文档摘要该函数支持多线程，是多线程安全的
-        参数：sTextFilename	    -[IN] 文档文件名
-            fSumRate	-[IN] 文档摘要占原文百分比（为0.00则不限制）
-            iSumLen		-[IN] 用户限定的摘要长度  （为0则不限制）
-            bHtmlTagRemove-[IN] 是否需要对原文进行Html标签的去除
-        返回：摘要字符串；出错返回空串
-        备注：在进程中此函数可以执行多次
-        :param text_filename:
-        :param sum_rate:
-        :param sum_len:
-        :param html_tag_remove:
-        :return:
+        Call **DS_FileProcess**
+
+        生成单文档摘要该函数支持多线程,是多线程安全的, make summarization from file with threading safe
+
+        :param str text_filename: 文档文件路径 text file path
+        :param float sum_rate: 文档摘要占原文百分比(为0.00则不限制）
+            the percentage of summarization length comparing to original text (0.00 represent no limit)
+        :param int sum_len: 用户限定的摘要长度(为0则不限制）The max len of summarization(0 will no limit)
+        :param bool html_tag_remove: 是否需要对原文进行Html标签的去除 remove the html tag or not
+        :return: 摘要字符串；出错返回空串 the summarization content, get null string if occurs error.
         """
         return self.get_func("DS_FileProcess", [c_char_p, c_float, c_int, c_int], c_char_p)(
-            text_filename, sum_rate, sum_len, html_tag_remove)
+            text_filename, c_float(sum_rate), sum_len, html_tag_remove)
