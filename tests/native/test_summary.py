42c0aa43c21dc431c9768d40c67a781d35580fb2
杨耀飞 <yangyaofei@gmail.com>
2020-11-19 17:05:28 +0800

    add test/summary nlpir/summary

---
+++ tests/native/test_summary.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+"""
+Tested Function:
+
+- :func:`nlpir.native.summary.Summary.exit_lib`
+- :func:`nlpir.native.summary.Summary.get_last_error_msg`
+- :func:`nlpir.native.summary.Summary.single_doc`
+- :func:`nlpir.native.summary.Summary.single_doc_e`
+- :func:`nlpir.native.summary.Summary.file_process`
+"""
+from nlpir.native import Summary
+from nlpir import native, clean_logs
+from ..strings import test_str, test_source_filename
+
+
+def get_summary(encode=native.UTF8_CODE):
+    return Summary(encode=encode)
+
+
+def test_init_exit():
+    summary = get_summary()
+    summary.exit_lib()
+    clean_logs(include_current=True)
+
+
+def test_summary_string():
+    summary = get_summary()
+    assert summary.single_doc(text=test_str, sum_rate=0.3, sum_len=50, html_tag_remove=True)
+    assert summary.single_doc_e(text=test_str, sum_rate=0.3, sum_len=50, html_tag_remove=True)
+
+
+def test_summary_file():
+    summary = get_summary()
+    assert summary.file_process(text_filename=test_source_filename, sum_rate=0.1, sum_len=300, html_tag_remove=True)
