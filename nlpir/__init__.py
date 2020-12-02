4c413fca5b7f3628c6a8eb3df3b1d92b6d517a04
杨耀飞 <yangyaofei@gmail.com>
2020-12-02 09:33:27 +0800

    bump to v0.0.2

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -7,7 +7,7 @@ import sys
 import functools
 from .exception import NLPIRException
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger("nlpir")
 
