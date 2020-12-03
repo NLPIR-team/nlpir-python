250f8a9e67878a8ae1e30a25f6cbe43555d77e36
杨耀飞 <yangyaofei@gmail.com>
2020-12-03 16:06:52 +0800

    add test/key_extract

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -3,4 +3,4 @@ from nlpir import summary
 
 def test_summarization():
     from tests.strings import test_str
-    summary.summarization(content=test_str, sum_rate=0.3, sum_len=50, html_tag_remove=True)
+    assert summary.summarization(content=test_str, sum_rate=0.3, sum_len=50, html_tag_remove=True)
