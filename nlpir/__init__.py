2b611f165ba3c9358650c8299868649d74383d74
杨耀飞 <yangyaofei@gmail.com>
2021-01-14 12:12:02 +0800

    bump version to 0.0.8

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -7,7 +7,7 @@ import sys
 import functools
 from .exception import NLPIRException
 
-__version__ = "0.0.7.2"
+__version__ = "0.0.8"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger("nlpir")
 
