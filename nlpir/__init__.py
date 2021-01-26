45b1ae771ec71637021d11d501eae28af9af89a7
杨耀飞 <yangyaofei@gmail.com>
2021-01-26 10:02:28 +0800

    bugfix for deep_classify and re-order from 0.0.9 to 0.9 and bump version to 0.9.1

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -7,7 +7,7 @@ import sys
 import functools
 from .exception import NLPIRException
 
-__version__ = "0.0.9"
+__version__ = "0.9.1"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger("nlpir")
 
