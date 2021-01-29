7619cd5de5385304ed9db8dbdcddfd6b9ba44587
杨耀飞 <yangyaofei@gmail.com>
2021-01-29 17:19:49 +0800

    #28 update sentiment_new for Darwin from commit https://github.com/NLPIR-team/NLPIR/commit/5fd8eb43eb4a9d2e7c26099a2d6161330f85afbd, bump version to v0.9.3

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -7,7 +7,7 @@ import sys
 import functools
 from .exception import NLPIRException
 
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger("nlpir")
 
