2046c6995f75282a41b98cfe83a71263cad797f5
杨耀飞 <yangyaofei@gmail.com>
2021-11-02 11:38:46 +0800

    bugfix for doc_extractor

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -7,7 +7,7 @@ import sys
 import functools
 from .exception import NLPIRException
 
-__version__ = "0.9.11"
+__version__ = "0.9.12"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger("nlpir")
 
