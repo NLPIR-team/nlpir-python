01d3fbe272363cc5c645b46335d65b998f5de8ce
杨耀飞 <yangyaofei@gmail.com>
2021-11-29 11:37:29 +0800

    remove wheel package for any because it exceeds the max size of pypi, polish code bump version to 0.9.14b2

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -7,7 +7,7 @@ import sys
 import functools
 from .exception import NLPIRException
 
-__version__ = "0.9.14b1"
+__version__ = "0.9.14b2"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger("nlpir")
 
