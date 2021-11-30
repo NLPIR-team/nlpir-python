006463cb95327440e8874b7bc47abc239138374d
杨耀飞 <yangyaofei@gmail.com>
2021-11-30 12:54:33 +0800

    bugfix for remove rules

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -7,7 +7,7 @@ import sys
 import functools
 from .exception import NLPIRException
 
-__version__ = "0.9.14b3"
+__version__ = "0.9.14b4"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger("nlpir")
 
