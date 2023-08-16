3f3bc12196236f1fa23b63848096c558015cde89
杨耀飞 <yangyaofei@gmail.com>
2023-08-16 13:09:58 +0800

    version 0.9.15.1

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -7,7 +7,7 @@ import sys
 import functools
 from .exception import NLPIRException
 
-__version__ = "0.9.15"
+__version__ = "0.9.15.1"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger("nlpir")
 
