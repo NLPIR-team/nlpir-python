37f3e9e097a3733d3b8c26670a36e91bb839b9a9
杨耀飞 <yangyaofei@gmail.com>
2023-11-16 10:10:10 +0800

    version to 0.9.15.2
    add wheel for all platforms

--- nlpir/__init__.py
+++ nlpir/__init__.py
@@ -7,7 +7,7 @@ import sys
 import functools
 from .exception import NLPIRException
 
-__version__ = "0.9.15.1"
+__version__ = "0.9.15.2"
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger("nlpir")
 
